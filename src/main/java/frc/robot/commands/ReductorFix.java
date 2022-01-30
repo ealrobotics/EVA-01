1f438fb8e6052c7f7dfb7de27395f1de5189e537
Talha <talha@karasu.xyz>
2022-01-31 00:46:53 +0300

    fix reductorFix command
    >add drive method to `execute` to prevent motors from stopping

--- src/main/java/frc/robot/commands/ReductorFix.java
+++ src/main/java/frc/robot/commands/ReductorFix.java
@@ -26,6 +26,7 @@ public class ReductorFix extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    drive.arcadeDrive(-0.5, 0);
   }
 
   // Called once the command ends or is interrupted.
